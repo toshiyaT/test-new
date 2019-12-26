1c66f1dbb8731239f40eebd74f3cb59cf5d58644
toshiyaT <58074993+toshiyaT@users.noreply.github.com>
2019-12-26 09:08:11 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,3 +22,33 @@ $(function(){
 
 });
 // header-nav
+
+// button-to-top
+jQuery(window).on("scroll", function($) {
+  if (jQuery(this).scrollTop() > 100) {
+    jQuery('.button-to-top').show();
+  } else {
+    jQuery('.button-to-top').hide();
+  }
+});
+
+jQuery('.button-to-top').click(function () {
+  jQuery('body,html').animate({
+    scrollTop: 0
+  }, 500);
+  return false;
+});
+// button-to-top end
+
+// スムーススクロール
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+// スムーススクロール end
